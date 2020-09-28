d2b1095a6203f30e217e450bde790e3cd8434602
JordanHardie <62230359+JordanHardie@users.noreply.github.com>
2020-09-28 20:21:42 +1000

    Bug
    mhm yes bug

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    private float _timeRemaining;
+    private float maxTime = 2 * 60;
+    public float TimeRemaining
+    {
+        get { return _timeRemaining;  }
+        set { _timeRemaining = value; }
+    }
+
+    void Start()
+    {
+        TimeRemaining = maxTime;
+    }
+
+    void Update()
+    {
+        TimeRemaining -= Time.deltaTime;
+
+        if (TimeRemaining <= 0)
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+    }
+}
